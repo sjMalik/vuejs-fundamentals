0a9dfc872ab8dc799ad1c4494b7bf70ac7e4f6c2
sjmalik <surajit.malik@qlikchain.com>
2023-04-26 12:46:57 +0530

    2023-04-26

---
+++ computed/example1.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Original Message: {{ message }}</p>
+    <p>Reverse Message: {{ reverseMessage }}</p>
+    <!-- <p>Reverse Message: {{ message.split('').reverse().join('') }}</p> -->
+</template>
+
+<script>
+    export default {
+        computed: {
+            reverseMessage: function() {
+                return this.message.split('').reverse().join('')
+            }
+        },
+        data(){
+            return {
+                message: 'Hello'
+            }
+        }
+    }
+</script>
---
+++ watch/example1.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>
+        Ask a question?
+        <input v-model="question" />
+    </p>
+    <p>{{ answer }}</p>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                question: '',
+                answer: ''
+            }
+        },
+        watch: {
+            question (newQuestion, oldQuestion){
+                if(newQuestion.includes('?') !== -1){
+                    this.getAnswer()
+                }
+            }
+        },
+        methods: {
+            async getAnswer(){
+                // HTTP requests
+            }
+        }
+    }
+</script>
